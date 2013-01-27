738a24a995155753c35e21b234eb8d3dbcbf578b
ElisDN <mail@elisdn.ru>
2013-01-27 20:37:17 +0400

    Added version

--- DCurrentPassword.php
+++ DCurrentPassword.php
@@ -4,6 +4,7 @@
  *
  * @author ElisDN <mail@elisdn.ru>
  * @link http://www.elisdn.ru/blog/10
+ * @version 1.0
  */
 
 class DCurrentPassword extends CValidator
@@ -29,8 +30,7 @@ class DCurrentPassword extends CValidator
         if (!$value)
             $this->addError($object, $attribute, $this->emptyMessage);
         elseif (!$model->{$this->validateMethod}($value))
-            $this->addError($object, $attribute, $this->notValidMessage,
-                array('{value}' => CHtml::encode($value)));
+            $this->addError($object, $attribute, $this->notValidMessage, array('{value}' => CHtml::encode($value)));
     }
 
     protected function checkDependsOnAttributes($object)
@@ -44,16 +44,20 @@ class DCurrentPassword extends CValidator
     {
         if (empty($this->idAttribute))
             throw new CException('Attribute idAttribute is not defined');
+
         if (empty($object->{$this->idAttribute}))
             throw new CException('Attribute ' . $this->idAttribute . ' not found');
+
         if (empty($this->validateMethod))
             throw new CException('Attribute validateMethod is not defined');
 
-        if (empty($this->className)) $this->className = get_class($object);
+        if (empty($this->className))
+            $this->className = get_class($object);
 
         $model = CActiveRecord::model($this->className)->findByPk($object->{$this->idAttribute});
 
-        if (is_null($model)) throw new CException('Model not found');
+        if (is_null($model))
+            throw new CException('Model not found');
 
         if (!method_exists($model, $this->validateMethod))
             throw new CException('Method ' . $this->className . '::' . $this->validateMethod . '() not found');
